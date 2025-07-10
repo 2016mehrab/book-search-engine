b7edc70d5ca97696288120d58029357b376c4508
Eshan <2016mehrab@gmail.com>
2025-07-10 14:34:59 +0600

    feat: Initial project setup (main)
    This commit sets up the initial project structure for the
    book search engine application. It includes:
    
    - pom.xml: Maven configuration file with dependencies.
    - .mvn/wrapper: Maven wrapper configuration.
    - mvnw, mvnw.cmd: Maven wrapper scripts.
    - .gitignore: Specifies intentionally untracked files.
    - src/main/java: Main application class.
    - src/test/java: Test class.
    - src/main/resources: application.properties and schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+create table books(
+	book_id serial primary key,
+	title varchar(255) not null,
+	rating decimal(2,1) not null,
+	description text not null,
+	language varchar(255) not null,
+	isbn varchar(255) not null,
+	book_format varchar(255) not null,
+	edition varchar(255) not null,
+	pages int not null,
+	publisher varchar(255) not null,
+	publish_date date not null,
+	first_publish_date date not null,
+	liked_percent decimal(5,2) not null,
+	price decimal(5,2) not null
+);
+
+create table authors(
+	author_id serial primary key,
+	name varchar(255) not null unique
+);
+
+create table books_authors(
+	author_id int primary key,
+	book_id int primary key,
+	primary key(book_id, author_id),
+	foreign key(book_id) references books (book_id),
+	foreign key(author_id) references authors (author_id)
+);
